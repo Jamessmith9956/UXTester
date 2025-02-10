cbf37f498dbc71159d05f1933670d99ab58a3896
Jamessmith9956 <Jamessmith9956@gmail.com>
2025-02-10 13:41:53 +1100

    demo broken start_left_drag functionality

--- main.lua
+++ main.lua
@@ -14,6 +14,12 @@ function main()
 end
 
 
+
+
+--I can get the drag to start correctly if I move the cursor from the current dialog into the graphics area before the drag starts, using an alert to pause the execution.
+
+--It will also start correctly if pyux.start_left_drag() is called from another click/drag handler, even if the cursor sits on top of the current dialog when the drag starts.
+
 function main_dialog(dialog_handle, data)
     dialog_handle:set_window_title(pyloc "UX Demo project")
     dialog_handle:equalize_column_widths({1,2})
@@ -21,29 +27,25 @@ function main_dialog(dialog_handle, data)
 
     local drag_btn = dialog_handle:create_button(1, pyloc "drag and drop")
     drag_btn:set_on_click_handler(function()
+        pyui.alert("Pause Execution, move cursor off of dialog before continuing")
         pyux.set_on_left_dragstart_handler(function (info) pyui.alert("this should never be reached") end)
         pyux.set_on_left_dragmove_handler(function (info) pyui.alert("here") end)
         pyux.set_on_left_dragend_handler(function (info) pyui.alert("where") end)
         pyux.start_left_drag()
     end)
 
-    local drag_drop_list = dialog_handle:create_drop_list(1)
-    drag_drop_list:insert_control_item(pyloc "a")
-    drag_drop_list:insert_control_item(pyloc "b")
-    drag_drop_list:set_on_change_handler(function(text, new_index)
-        pyui.run_modal_subdialog(function(dialog)
-            pyux.set_on_left_dragstart_handler(function (info) pyui.alert("this should never be reached") end)
-            pyux.set_on_left_dragmove_handler(function (info) pyui.alert("here") end)
-            pyux.set_on_left_dragend_handler(function (info) pyui.alert("where") end)
-            -- pyux.set_on_left_dragend_handler(function (info) pyux.set_on_left_dragstart_handler() end)
-            pyux.set_on_left_click_handler(function(info)
-                pyux.set_on_left_dragstart_handler(nil)
-                pyux.set_on_left_dragmove_handler(nil)
-                pyux.set_on_left_dragend_handler(nil)
-            end)
-            pyux.show_cursor_crosshair(true)
-            -- pyux.start_left_drag()
-        end)
-    end)
+    pyux.set_on_left_click_handler(function(info) pyui.run_modal_subdialog(function(dialog)
+        dialog:set_window_title("UX Tester Obstruction Window")
+        dialog:create_label(1, "")
+        dialog:create_label(1, "")
+        dialog:create_label(1, "")
+        dialog:create_label(1, "")
+        dialog:create_button(1, pyloc "Dummy button")
+        
+        pyux.set_on_left_dragstart_handler(function (info) pyui.alert("this should never be reached") end)
+        pyux.set_on_left_dragmove_handler(function (info) pyui.alert("here") end)
+        pyux.set_on_left_dragend_handler(function (info) pyui.alert("where") end)
+        pyux.start_left_drag()
+    end)end)
 
 end
