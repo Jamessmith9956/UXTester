285750ee2e8f23a6518fb3e7b7d3fab317c65f80
Jamessmith9956 <Jamessmith9956@gmail.com>
2025-04-30 15:15:42 +1000

    refigure as part demo

--- main.lua
+++ main.lua
@@ -24,28 +24,67 @@ function main_dialog(dialog_handle, data)
     dialog_handle:set_window_title(pyloc "UX Demo project")
     dialog_handle:equalize_column_widths({1,2})
     -- UX handlers
+    local log_a = dialog_handle:create_text_box(1)
+    log_a:enable_control(false)
+
+    local ux_part_name_lookup = {
+        {name=pyloc "Arrow 3D", func=ux_arrow_3d},
+        {name=pyloc "Bounded Arrow 3D", func=ux_bounded_arrow_3d},
+        {name=pyloc "Double Chevron 3D", func=ux_double_chevron_3d},
+        {name=pyloc "Line 3D", func=ux_line_3d},
+    }
+    local ux_part_selection = dialog_handle:create_list_box({1,2}, pyloc "UX Parts")
+    for i, k in ipairs(ux_part_name_lookup) do
+        ux_part_selection:insert_control_item(k.name)
+    end
+    local current_ux_part_idx = 1
+    ux_part_selection:set_control_selection(current_ux_part_idx)
 
     local drag_btn = dialog_handle:create_button(1, pyloc "drag and drop")
     drag_btn:set_on_click_handler(function()
-        pyui.alert("Pause Execution, move cursor off of dialog before continuing")
-        pyux.set_on_left_dragstart_handler(function (info) pyui.alert("this should never be reached") end)
-        pyux.set_on_left_dragmove_handler(function (info) pyui.alert("here") end)
-        pyux.set_on_left_dragend_handler(function (info) pyui.alert("where") end)
+        drag_btn.state = not drag_btn.state
+        local origin_vp, terminus_vp = nil, nil
+        pyux.set_on_left_dragstart_handler(function (info) end)
+        pyux.set_on_left_dragmove_handler(function (info) log_a:set_control_text(table_tostring(info.coos_vp)) end)
+        pyux.set_on_left_dragend_handler(function (info)
+            origin_vp = info.coos_vp
+            pyux.set_on_left_dragstart_handler(function (info) end)
+            pyux.set_on_left_dragmove_handler(function(info) end)
+            pyux.set_on_left_dragend_handler(function(info) 
+                terminus_vp = info.coos_vp
+                local origin = pyux.identify_coordinate_on_plane(origin_vp, {0,0,0}, {0,0,1})
+                local terminus = pyux.identify_coordinate_on_plane(terminus_vp, {0,0,0}, {0,0,1})
+                local u_axis = {
+                    terminus[1] - origin[1],
+                    terminus[2] - origin[2],
+                    terminus[3] - origin[3],
+                }
+                local length = PYTHAGORAS(u_axis[1], u_axis[2], u_axis[3])
+                if length>0 then
+                    u_axis = {
+                        u_axis[1]/length,
+                        u_axis[2]/length,
+                        u_axis[3]/length,
+                    }
+                end
+
+                local func = ux_part_name_lookup[current_ux_part_idx].func
+                if func then
+                    local part = func(nil, origin, {u_axis=u_axis}, nil, nil, nil)
+                    table.insert(data.parts, part)
+                    if not data.main_group then
+                        data.main_group = pytha.create_group(part, {name=pyloc"main_group"})
+                    else
+                        pytha.set_element_group(part, data.main_group)
+                    end
+                end
+            end)
+            pyux.start_left_drag()
+        end)
         pyux.start_left_drag()
     end)
 
-    pyux.set_on_left_click_handler(function(info) pyui.run_modal_subdialog(function(dialog)
-        dialog:set_window_title("UX Tester Obstruction Window")
-        dialog:create_label(1, "")
-        dialog:create_label(1, "")
-        dialog:create_label(1, "")
-        dialog:create_label(1, "")
-        dialog:create_button(1, pyloc "Dummy button")
-        
-        pyux.set_on_left_dragstart_handler(function (info) pyui.alert("this should never be reached") end)
-        pyux.set_on_left_dragmove_handler(function (info) pyui.alert("here") end)
-        pyux.set_on_left_dragend_handler(function (info) pyui.alert("where") end)
-        pyux.start_left_drag()
-    end)end)
-
+    ux_part_selection:set_on_change_handler(function (text, index)
+        current_ux_part_idx = index
+    end)
 end
